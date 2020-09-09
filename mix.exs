0af61746d775dc7f5ea57baf3ff0e4bc3492a009
Piotr Laszewski <laszpio@gmail.com>
2020-09-09 15:44:54 +0200

    Gems: added Hui for handling Solr

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule DgDemo.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:hui, "~> 0.10.2"}
     ]
   end
 
