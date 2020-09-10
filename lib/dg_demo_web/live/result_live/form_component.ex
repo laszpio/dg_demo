375aac8c8f5439538814707a4e3a54e86b7100ba
Piotr Laszewski <laszpio@gmail.com>
2020-09-10 12:24:07 +0200

    ResultLive: remove redundant form_components.

--- lib/dg_demo_web/live/result_live/form_component.ex
+++ lib/dg_demo_web/live/result_live/form_component.ex
@@ -4,16 +4,6 @@ defmodule DgDemoWeb.ResultLive.FormComponent do
   alias DgDemo.Search
 
   @impl true
-  def update(%{result: result} = assigns, socket) do
-    changeset = Search.change_result(result)
-
-    {:ok,
-     socket
-     |> assign(assigns)
-     |> assign(:changeset, changeset)}
-  end
-
-  @impl true
   def handle_event("validate", %{"result" => result_params}, socket) do
     changeset =
       socket.assigns.result
@@ -21,35 +11,5 @@ defmodule DgDemoWeb.ResultLive.FormComponent do
       |> Map.put(:action, :validate)
 
     {:noreply, assign(socket, :changeset, changeset)}
-  end
-
-  def handle_event("save", %{"result" => result_params}, socket) do
-    save_result(socket, socket.assigns.action, result_params)
-  end
-
-  defp save_result(socket, :edit, result_params) do
-    case Search.update_result(socket.assigns.result, result_params) do
-      {:ok, _result} ->
-        {:noreply,
-         socket
-         |> put_flash(:info, "Result updated successfully")
-         |> push_redirect(to: socket.assigns.return_to)}
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply, assign(socket, :changeset, changeset)}
-    end
-  end
-
-  defp save_result(socket, :new, result_params) do
-    case Search.create_result(result_params) do
-      {:ok, _result} ->
-        {:noreply,
-         socket
-         |> put_flash(:info, "Result created successfully")
-         |> push_redirect(to: socket.assigns.return_to)}
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply, assign(socket, changeset: changeset)}
-    end
   end
 end
