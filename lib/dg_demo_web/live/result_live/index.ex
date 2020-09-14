3f9464c2d4d0a2b7fb2e79e70c647fd7343bd8b0
Piotr Laszewski <laszpio@gmail.com>
2020-09-14 09:23:15 +0200

    Search: handle when opened with search params

--- lib/dg_demo_web/live/result_live/index.ex
+++ lib/dg_demo_web/live/result_live/index.ex
@@ -8,16 +8,18 @@ defmodule DgDemoWeb.ResultLive.Index do
     {:ok, assign(socket, query: "", search: Search.search())}
   end
 
-  #def handle_params(params, _url, socket) do
-  #  {:noreply, apply_action(socket, socket.assigns.live_action, params)}
-  #end
+  def handle_params(params, _url, socket) do
+    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
+  end
 
-  #defp apply_action(socket, :index, %{"q" => q}) do
-  #  socket
-  #  |> assign(:page_title, "Listing Results")
-  #  |> assign(:query, q)
-  #  |> assign(:search, Search.search(q))
-  #end
+  defp apply_action(socket, :index, %{"q" => q}) do
+    socket
+    |> assign(:page_title, "Listing Results")
+    |> assign(:query, q)
+    |> assign(:search, Search.search(q))
+  end
+
+  defp apply_action(socket, :index, _params), do: socket
 
   def handle_event("search", %{"q" => q}, socket) do
     {:noreply, assign(socket, query: q, search: Search.search(q))}
