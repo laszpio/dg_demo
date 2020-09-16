101adbffff68b4ba117a30dd53aa78cdfa28a3ed
Piotr Laszewski <laszpio@gmail.com>
2020-09-16 14:43:41 +0200

    Result: removed new/1 and to_struct/2

--- lib/dg_demo/search/result.ex
+++ lib/dg_demo/search/result.ex
@@ -16,22 +16,7 @@ defmodule DgDemo.Search.Result do
 
   @allowed_fields [:domain, :slug, :title, :tags, :authors, :created_at]
 
-  def new(result) do
-    to_struct(Result, result)
-  end
-
   def changeset(changeset, params) do
     cast(%Result{}, params, @allowed_fields)
-  end
-
-  defp to_struct(kind, attrs) do
-    struct = struct(kind)
-
-    Enum.reduce Map.to_list(struct), struct, fn {k, _}, acc ->
-      case Map.fetch(attrs, Atom.to_string(k)) do
-        {:ok, v} -> %{acc | k => v}
-        :error -> acc
-      end
-    end
   end
 end
