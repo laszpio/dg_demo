5179af36a08c0429c36d6b4cfbd4005e688c650e
Piotr Laszewski <laszpio@gmail.com>
2020-09-17 10:36:12 +0200

    Search test: mocking Search.Count.total_count/0

--- test/dg_demo/search_test.exs
+++ test/dg_demo/search_test.exs
@@ -4,30 +4,18 @@ defmodule DgDemo.SearchTest do
 
   alias DgDemo.Search
 
+
   describe "search" do
     @empty %Search{count: 0, total: 42, results: []}
 
     test "search/0 returns empty search result" do
-      response = %{
-        body: %{
-          "response" => %{"numFound" => 42}
-        }
-      }
-
-      with_mock Hui, [search: fn _, q: "*", rows: 0 -> {:ok, response} end] do
+      with_mock Search.Count, [total_count: fn -> 42 end] do
         assert Search.search() == @empty
       end
     end
 
     test "search/1 with empty query returns empty search result" do
-      response = %{
-        body: %{
-          "response" => %{"numFound" => 42, "docs" => []},
-          "responseHeader" => %{"QTime" => 0}
-        }
-      }
-
-      with_mock Hui, [search: fn _, _ -> {:ok, response} end] do
+      with_mock Search.Count, [total_count: fn -> 42 end] do
         assert Search.search(nil) == @empty
         assert Search.search("") == @empty
       end
