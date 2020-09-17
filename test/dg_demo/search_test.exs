7614ec7d6e5a9e5cbb7cb80a9319911c03921b73
Piotr Laszewski <laszpio@gmail.com>
2020-09-17 09:11:35 +0200

    SearchTest: use mock for empty response.

--- test/dg_demo/search_test.exs
+++ test/dg_demo/search_test.exs
@@ -8,18 +8,30 @@ defmodule DgDemo.SearchTest do
     @empty %Search{count: 0, total: 42, results: []}
 
     test "search/0 returns empty search result" do
-      response = %{body: %{"response" => %{"numFound" => 42}}}
+      response = %{
+        body: %{
+          "response" => %{"numFound" => 42}
+        }
+      }
 
       with_mock Hui, [search: fn _, q: "*", rows: 0 -> {:ok, response} end] do
         assert Search.search() == @empty
       end
     end
 
-    @tag :skip
     test "search/1 with empty query returns empty search result" do
-      assert Search.search(nil) == @empty
-      assert Search.search("") == @empty
-      assert Search.search("  ") == @empty
+      response = %{
+        body: %{
+          "response" => %{"numFound" => 42, "docs" => []},
+          "responseHeader" => %{"QTime" => 0}
+        }
+      }
+
+      with_mock Hui, [search: fn _, _ -> {:ok, response} end] do
+        assert Search.search(nil) == @empty
+        assert Search.search("") == @empty
+        assert Search.search("  ") == @empty
+      end
     end
   end
 end
