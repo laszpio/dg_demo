b8a99f7ad6656317102cacee19ee63a2f3132c72
Piotr Laszewski <laszpio@gmail.com>
2020-09-17 12:40:05 +0200

    Search/1 with term: mocking results.

--- test/dg_demo/search_test.exs
+++ test/dg_demo/search_test.exs
@@ -24,5 +24,27 @@ defmodule DgDemo.SearchTest do
         assert called Search.Count.total_count()
       end
     end
+
+    test "search/1 with single term" do
+      response = %{
+        body: %{
+          "response" => %{
+            "numFound" => 3,
+            "docs" => []
+          },
+          "responseHeader" => %{"QTime" => "2"}
+        }
+      }
+
+      with_mock Search.Count, [total_count: fn -> 42 end] do
+        with_mock Hui, [search: fn(_, _) -> {:ok, response} end] do
+          assert Search.search("term") == %Search{
+            total: 42,
+            count: 3,
+            time: 2
+          }
+        end
+      end
+    end
   end
 end
