ca5d732e066f5dbe4dd5216aaf98bcd3f25d67e7
Piotr Laszewski <laszpio@gmail.com>
2020-09-18 13:26:15 +0200

    Search.Config tests for core/0, path/0, headers/0.

--- test/dg_demo/search/config_test.exs
+++ test/dg_demo/search/config_test.exs
@@ -18,4 +18,12 @@ defmodule DgDemo.Search.ConfigTest do
   test "core/0" do
     assert Config.core() == "test_items"
   end
+
+  test "path/0" do
+    assert Config.path() == "http://localhost:8983/solr/test_items"
+  end
+
+  test "headers/0 returns default client headers" do
+    assert {"Content-type", "application/json"} in Config.headers
+  end
 end
