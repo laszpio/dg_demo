66d22e76aff1d17863d142af3e7ef92778d0cf24
Piotr Laszewski <laszpio@gmail.com>
2020-10-28 20:03:18 +0100

    Searching: show pagination container on submit

--- lib/dg_demo_web/live/result_live/index.ex
+++ lib/dg_demo_web/live/result_live/index.ex
@@ -1,10 +1,12 @@
 defmodule DgDemoWeb.ResultLive.Index do
   use DgDemoWeb, :live_view
 
+  require Logger
+
   alias DgDemo.Search
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, query: "", search: Search.search())}
+    {:ok, assign(socket, query: "", search: Search.search(), pagination: false)}
   end
 
   def handle_params(params, _url, socket) do
@@ -13,7 +15,8 @@ defmodule DgDemoWeb.ResultLive.Index do
 
   defp apply_action(socket, :index, %{"q" => q}) do
     socket
-    |> assign(:page_title, "Listing Results")
+    |> assign(:page_title, "Results")
+    |> assign(:pagination, false)
     |> assign(:query, q)
     |> assign(:search, Search.search(q))
   end
@@ -24,7 +27,9 @@ defmodule DgDemoWeb.ResultLive.Index do
     socket =
       socket
       |> assign(:query, "")
+      |> assign(:page_title, "Searching")
       |> assign(:search, Search.search())
+      |> assign(:pagination, false)
       |> push_patch(to: Routes.result_index_path(socket, :index))
 
     {:noreply, socket}
@@ -33,9 +38,17 @@ defmodule DgDemoWeb.ResultLive.Index do
   def handle_event("search", %{"q" => q}, socket) do
     socket =
       socket
+      |> assign(:page_title, "Searching")
       |> assign(:query, q)
       |> assign(:search, Search.search(q))
+      |> assign(:pagination, false)
       |> push_patch(to: Routes.result_index_path(socket, :index, q: q))
+
+    {:noreply, socket}
+  end
+
+  def handle_event("show", %{"q" => q}, socket) do
+    socket = assign(socket, :pagination, true)
 
     {:noreply, socket}
   end
